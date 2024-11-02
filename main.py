fa3b6f841950e8fea2cd49d5cf64de24dcc6c789
othermully <ryancbmullins6@gmail.com>
2024-11-02 18:24:05 -0300

    Fixed issue with dict not being updated

--- main.py
+++ main.py
@@ -4,26 +4,28 @@ import pandas as pd
 file_path = "./output.json"
 
 def main():
-    parse_data()
-
+    create_dataframe()
 def read_file(path):
     with open(path, 'r') as file:
         data = json.load(file)
     return data
 
 def parse_data():
-    data_dict = {}
     data_dict_list = []
     data = read_file(file_path)
     for single_item_list in data["data"]:
-        data_dict["date"] = single_item_list[0]
-        data_dict["price"] = single_item_list[1]
-        data_dict["stock"] = single_item_list[2]
-        data_dict["volume"] = single_item_list[3]
-        data_dict_list.append(data_dict) 
+        #Creating a new dict for each item
+        record = {
+            'date': single_item_list[0],
+            'price': single_item_list[1],
+            'stock': single_item_list[2],
+            'volume': single_item_list[3]
+        }
+        data_dict_list.append(record)
     return data_dict_list
 
 def create_dataframe():
-    pass
+    df = pd.DataFrame(parse_data())
+    print(df)
 
 main()
